6ce0f611063db586dcd8e6abb5b58aad4146a59e
rafahuelin <rafahuelin@gmail.com>
2021-12-28 16:57:47 +0100

    add admin list filterable by tag

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ('slug', 'image', 'tags', 'content', 'creation_date', )
+    list_display = ('slug', 'tag_list', 'creation_date', )
+    list_filter = ('tags', )
+    readonly_fields = ('creation_date', )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
+
+
+admin.site.register(Article, ArticleAdmin)
